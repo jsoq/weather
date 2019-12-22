41faa5d5a4536d834f2f88e5bc7b86eea0cbf71e
Jsom <775480484@qq.com>
2019-12-23 00:46:25 +0800

    first commit

---
+++ src/Exceptions/Exception.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace Jsoq\Weather\Exceptions;
+
+class Exception extends \Exception
+{
+
+}
---
+++ src/Exceptions/HttpException.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace Jsoq\Weather\Exceptions;
+
+class HttpException extends Exception
+{
+
+}
